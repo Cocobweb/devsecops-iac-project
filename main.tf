c0a91df1e40e7a1c0764d044afb59297db33841c
Boutault <corentin@kali.cobweb_kali.com>
2026-03-12 23:12:03 +0000

    Revert "test: simulate SSH exposure to test pipeline blocking"
    This reverts commit 8ea403b221ccaa1947ff76f82e51a2ab589ecffa.

--- main.tf
+++ main.tf
@@ -587,15 +587,3 @@ resource "aws_cloudtrail" "main" {
     Name = "devsecops-trail"
   }
 }
-
-resource "aws_security_group" "bad_test" {
-  name_prefix = "test-fail-"
-  description = "test"
-  vpc_id      = aws_vpc.main.id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
